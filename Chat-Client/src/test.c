19817388ceae4007ace1cbc97547f1211cea9fa9
Michael Gordon <mgordon6900@conestogac.on.ca>
2020-04-06 16:13:02 -0400

    Backspace handled with getch. :bug:

--- Chat-Client/src/test.c
+++ Chat-Client/src/test.c
@@ -4,19 +4,6 @@
 
 #define INPUT_MAX   81
 #define BOX_WIDTH   70
-
-
-void rectangle(int y1, int x1, int y2, int x2)
-{
-    mvhline(y1, x1, 0, x2-x1);
-    mvhline(y2, x1, 0, x2-x1);
-    mvvline(y1, x1, 0, y2-y1);
-    mvvline(y1, x2, 0, y2-y1);
-    mvaddch(y1, x1, ACS_ULCORNER);
-    mvaddch(y2, x1, ACS_LLCORNER);
-    mvaddch(y1, x2, ACS_URCORNER);
-    mvaddch(y2, x2, ACS_LRCORNER);
-}
 void blankWin(WINDOW *win)
 {
   int i;
@@ -30,14 +17,14 @@ void blankWin(WINDOW *win)
     wclrtoeol(win);
     wrefresh(win);
   }
-  //box(win, 0, 0);             /* draw the box again */
+  box(win, 0, 0);             /* draw the box again */
   wrefresh(win);
 }
 void input_win(WINDOW *win, char *word)
 {
   int i, ch;
   int maxrow, maxcol, row = 1, col = 0;
-
+  int y, x;
   blankWin(win);                          /* make it a clean window */
   getmaxyx(win, maxrow, maxcol);          /* get window size */
   bzero(word, INPUT_MAX);
@@ -45,37 +32,65 @@ void input_win(WINDOW *win, char *word)
   //mvwgetnstr(win, 1, 1, word, 80);
   for (i = 0; (ch = wgetch(win)) != '\n'; i++)
   {
-    if(ch >= 32 && ch < 127)
+    switch(ch)
     {
-
-      if(strlen(word) < (INPUT_MAX - 1))
+      case KEY_UP:
+      break;
+      case KEY_DOWN:
+      break;
+      case KEY_LEFT:
+      break;
+      case KEY_RIGHT:
+      break;
+      default:
+      if(ch == KEY_BACKSPACE || ch == KEY_DC || ch == 127)
       {
-        word[i] = ch;                       /* '\n' not copied */
-        if (col++ < maxcol-2)               /* if within window */
+        getyx(win, y, x);
+        if(x != 1)
         {
-          wprintw(win, "%c", word[i]);      /* display the char recv'd */
+        wmove(win, y, x-1);
+        wprintw(win, " ");
+        wmove(win, y, x-1);
         }
-        else                                /* last char pos reached */
-        {
-          col = 1;
-          if (row == maxrow-2)              /* last line in the window */
-          {
-            scroll(win);                    /* go up one line */
-            row = maxrow-2;                 /* stay at the last line */
-            wmove(win, row, col);           /* move cursor to the beginning */
-            wclrtoeol(win);                 /* clear from cursor to eol */
-            box(win, 0, 0);                 /* draw the box again */
-          }
-          else
-          {
-            row++;
-            wmove(win, row, col);           /* move cursor to the beginning */
-            wrefresh(win);
-            wprintw(win, "%c", word[i]);    /* display the char recv'd */
-          }
-        }
+        i--;
+        word[i] = 0;
+        i--;
+        wrefresh(win);
       }
+      if(ch >= 32 && ch < 127)
+      {
+
+        if(strlen(word) < (INPUT_MAX - 1))
+        {
+          word[i] = ch;                       /* '\n' not copied */
+          if (col++ < maxcol-2)               /* if within window */
+          {
+            wprintw(win, "%c", word[i]);      /* display the char recv'd */
+          }
+          else                                /* last char pos reached */
+          {
+            col = 1;
+            if (row == maxrow-2)              /* last line in the window */
+            {
+              scroll(win);                    /* go up one line */
+              row = maxrow-2;                 /* stay at the last line */
+              wmove(win, row, col);           /* move cursor to the beginning */
+              wclrtoeol(win);                 /* clear from cursor to eol */
+              box(win, 0, 0);                 /* draw the box again */
+            }
+            else
+            {
+              row++;
+              wmove(win, row, col);           /* move cursor to the beginning */
+              wrefresh(win);
+              wprintw(win, "%c", word[i]);    /* display the char recv'd */
+            }
+          }
+        }
     }
+
+    }
+
   }
 }
 int main()
@@ -108,15 +123,16 @@ int main()
   init_color(COLOR_WHITE,933,933, 933);
 
   //position subwindow based on x and y of parent window
-  rectangle(y-11,0,y-1 ,x-1);
-  WINDOW* subwindow = newwin(9,x-2,(y-10),1);
+  WINDOW* subwindow = newwin(10,x,(y-10),0);
   scrollok(subwindow,TRUE);
   wbkgd(subwindow, COLOR_PAIR(2));
-
+  keypad(subwindow, 1);
   refresh();
 
   //Should color change when new message received?
+  printw("New Message\n");
   refresh();
+  box(subwindow,0,0);
   char str[INPUT_MAX];
   //mvwprintw(subwindow, 1, 1, "subwindow");
   wrefresh(subwindow);
