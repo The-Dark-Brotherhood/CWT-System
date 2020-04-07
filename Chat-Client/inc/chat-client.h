d62ff55aa527a96cdc112e0bcc7503dcd4a3179c
Michael Gordon <mgordon6900@conestogac.on.ca>
2020-04-07 15:40:05 -0400

    ui adjustment :lipstick: command line arguments :sparkles:

--- Chat-Client/inc/chat-client.h
+++ Chat-Client/inc/chat-client.h
@@ -1,12 +1,30 @@
 #pragma once
 #include <ncurses.h>
 #include <curses.h>
-#include <string.h>
-#include <unistd.h>
 #include <signal.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <ifaddrs.h>
+#include <netdb.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+    #include <linux/if_link.h>
 
-#define INPUT_MAX   81
-#define BOX_WIDTH   70
+
+#define INPUT_MAX     81
+#define BOX_WIDTH     70
+#define MESSAGE_SIZE  79
+#define PORT          5000
+#define IP_SIZE       20
+
 
 void blankWin(WINDOW *win);
 void setUpWindows(WINDOW *subwindow, WINDOW *subBackground, WINDOW *messagesWindow, WINDOW *messagesWindowBackground);
--- Chat-Client/src/chat-client.c
+++ Chat-Client/src/chat-client.c
@@ -2,8 +2,72 @@
 
 //Recommended to take in arguments for user and server
 
-int main()
+int main(int argc, char *argv[])
 {
+  int                my_server_socket, len, done;
+  int                whichClient;
+  struct sockaddr_in server_addr;
+  struct hostent*    host;
+
+  //https://stackoverflow.com/questions/4139405/how-can-i-get-to-know-the-ip-address-for-interfaces-in-c
+  struct ifaddrs *ifap, *ifa;
+  struct sockaddr_in *sa;
+  char *addr;
+  char clientIP[IP_SIZE] = "";
+  getifaddrs (&ifap);
+  for (ifa = ifap; ifa; ifa = ifa->ifa_next) {
+      if (ifa->ifa_addr && ifa->ifa_addr->sa_family==AF_INET) {
+          sa = (struct sockaddr_in *) ifa->ifa_addr;
+          addr = inet_ntoa(sa->sin_addr);
+          if(!strcmp(ifa->ifa_name, "ens33"))
+          {
+            strcpy(clientIP, addr);
+          }
+          //printf("Interface: %s\tAddress: %s\n", ifa->ifa_name, addr);
+      }
+  }
+
+  freeifaddrs(ifap);
+  printf("%s", clientIP);
+  return 0;
+  /* display result */
+  //printf("%s\n", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+  //strcat(clientIP, inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+  if (argc != 3)
+  {
+    printf ("USAGE : chat-client <user> <server_name>\n");
+    return 1;
+  }
+
+  if(strlen(argv[1]) > 5)
+  {
+    printf("User name length must be 5 characters max\n");
+    return 2;
+  }
+
+  /*
+   * determine host info for server name supplied
+   */
+  if ((host = gethostbyname(argv[2])) == NULL)
+  {
+    printf ("[CLIENT-%d] : Host Info Search - FAILED\n",whichClient);
+    return 3;
+  }
+
+  memset (&server_addr, 0, sizeof (server_addr));
+  server_addr.sin_family = AF_INET;
+  memcpy (&server_addr.sin_addr, host->h_addr, host->h_length);
+  server_addr.sin_port = htons (PORT);
+
+  printf ("[CLIENT-%d] : Getting STREAM Socket to talk to SERVER\n", whichClient);
+  fflush(stdout);
+       if ((my_server_socket = socket (AF_INET, SOCK_STREAM, 0)) < 0)
+       {
+         printf ("[CLIENT-%d] : Getting Client Socket - FAILED\n", whichClient);
+         return 4;
+       }
+
+
 
   initscr();
   cbreak();
@@ -54,17 +118,15 @@ int main()
       strcat(input, str);
 
       resizeWindows(subwindow, subBackground, messagesWindow, messagesWindowBackground);
-      //wcursyncup(subwindow);
     }
-
-
-    // bkgd(COLOR_PAIR(1));
-    // wbkgd(subwindow, COLOR_PAIR(2));
-
-
+    char outgoingMsg[MESSAGE_SIZE] = "";
+    //sprintf(outgoingMsg, "");
   }
 
   getch();
+  delwin(subBackground);
+  delwin(messagesWindow);
+  delwin(messagesWindowBackground);
   delwin(subwindow);
 
   endwin();
